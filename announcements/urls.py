64d32ceebd7202d5e3232f70d12a30ff8f35d252
robsta7 <meremeev2001@gmail.com>
2020-11-13 22:17:23 +0300

    Обновлён файл urls.py

--- announcements/urls.py
+++ announcements/urls.py
@@ -4,10 +4,34 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('announcement/feed/', FeedAnnouncementListAPIView.as_view(), name='feed-announcements'),
-    path('announcement/create/', AnnouncementCreateAPIView.as_view(), name='create-announcement'),
-    path('announcement/delete/<int:pk>/', AnnouncementDeleteAPIView.as_view(), name='delete-announcement'),
-    path('announcement/my/', MyAnnouncementListAPIView.as_view(), name='my-announcements'),
-    path('announcement/retrieve/<int:pk>/', AnnouncementRetrieveAPIView.as_view(), name='retrieve-announcement'),
-    path('announcement/feed-map-info/', FeedMapInfoListAPIView.as_view(), name='feed-map-info'),
+    path(
+        'announcement/feed/',
+        FeedAnnouncementListAPIView.as_view(),
+        name='feed-announcements'
+    ),
+    path(
+        'announcement/create/',
+        AnnouncementCreateAPIView.as_view(),
+        name='create-announcement'
+    ),
+    path(
+        'announcement/delete/<int:pk>/',
+        AnnouncementDeleteAPIView.as_view(),
+        name='delete-announcement'
+    ),
+    path(
+        'announcement/my/',
+        MyAnnouncementListAPIView.as_view(),
+        name='my-announcements'
+    ),
+    path(
+        'announcement/retrieve/<int:pk>/',
+        AnnouncementRetrieveAPIView.as_view(),
+        name='retrieve-announcement'
+    ),
+    path(
+        'announcement/feed-map-info/',
+        FeedMapInfoListAPIView.as_view(),
+        name='feed-map-info'
+    ),
 ]
