eb0bad98bfedb87c2fade085d02cf55a34c23930
robsta7 <meremeev2001@gmail.com>
2020-11-13 16:16:27 +0300

    Добавлена схема

--- config/settings/base.py
+++ config/settings/base.py
@@ -89,6 +89,9 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.MultiPartParser',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
