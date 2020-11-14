c724398e5b998e9dea325de432e4779bca58bb30
robsta7 <meremeev2001@gmail.com>
2020-11-14 03:48:52 +0300

    -redoc

--- config/urls.py
+++ config/urls.py
@@ -31,11 +31,6 @@ urlpatterns = [
         schema_view.with_ui('swagger', cache_timeout=0),
         name='schema-swagger-ui'
     ),
-    path(
-        'api/redoc/',
-        schema_view.with_ui('redoc', cache_timeout=0),
-        name='schema-redoc'
-    ),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
