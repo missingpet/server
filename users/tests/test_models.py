ae79957d39e530c4cb2091c3c0630b9504608d9b
robsta7 <meremeev2001@gmail.com>
2020-11-20 21:12:46 +0300

    Добавлены заготовки для тестов модели пользователя

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -1,14 +1,39 @@
-# from django.test import TestCase
-# from users.models import User
-# class UserTestCases(TestCase):
-#     def setUp(self):
-#         self.user = User.objects.create_user(
-#             username='user',
-#             email='user@email.com',
-#             password='123'
-#         )
-#         self.superuser=User.objects.create_superuser(
-#             username='superuser',
-#             email='user@email.com',
-#             password='456'
-#         )
+from django.test import TestCase
+
+from users.models import User
+
+
+class UserTestCases(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='user',
+            email='user@email.com',
+            password='123'
+        )
+        self.superuser = User.objects.create_superuser(
+            username='superuser',
+            email='superuser@email.com',
+            password='456'
+        )
+
+    def test_users_count(self):
+        pass
+
+    def test_username(self):
+        pass
+
+    def test_email(self):
+        pass
+
+    def test_password(self):
+        pass
+
+    def test_is_active(self):
+        pass
+
+    def test_user_rights(self):
+        pass
+
+    def tearDown(self):
+        pass
