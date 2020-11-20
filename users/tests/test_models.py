c9af937bce9092d68590788b0fce4a6915c9f16c
senichevleonid@yandex.ru <senichevleonid@yandex.ru>
2020-11-20 20:16:30 +0300

    Закомминтил всё что написал для тестов user и superuser
    Signed-off-by: senichevleonid@yandex.ru <senichevleonid@yandex.ru>

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -1,1 +1,14 @@
-from django.test import TestCase
+# from django.test import TestCase
+# from users.models import User
+# class UserTestCases(TestCase):
+#     def setUp(self):
+#         self.user = User.objects.create_user(
+#             username='user',
+#             email='user@email.com',
+#             password='123'
+#         )
+#         self.superuser=User.objects.create_superuser(
+#             username='superuser',
+#             email='user@email.com',
+#             password='456'
+#         )
