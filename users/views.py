04862dd097461cef1045dfd32f1fd26981152350
robsta7 <meremeev2001@gmail.com>
2020-12-17 10:28:21 +0300

    Fix typo

--- users/views.py
+++ users/views.py
@@ -3,7 +3,6 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.status import HTTP_200_OK
 from rest_framework.status import HTTP_201_CREATED
 from rest_framework.status import HTTP_204_NO_CONTENT
-
 from rest_framework.response import Response
 
 from rest_framework.permissions import IsAuthenticated
