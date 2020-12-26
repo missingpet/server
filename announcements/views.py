1fdaa8ad04a426ad092f58f723a9caf016d1ad35
Restyled.io <commits@restyled.io>
2020-12-26 13:13:41 +0000

    Restyled by reorder-python-imports

--- announcements/views.py
+++ announcements/views.py
@@ -1,11 +1,15 @@
-from rest_framework.generics import (CreateAPIView, DestroyAPIView,
-                                     ListAPIView, RetrieveAPIView)
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.generics import CreateAPIView
+from rest_framework.generics import DestroyAPIView
+from rest_framework.generics import ListAPIView
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Announcement
 from .permissions import IsAnnouncementAuthor
-from .serializers import (AnnouncementCreateSerializer,
-                          AnnouncementRetrieveSerializer, MapInfoSerializer)
+from .serializers import AnnouncementCreateSerializer
+from .serializers import AnnouncementRetrieveSerializer
+from .serializers import MapInfoSerializer
 
 
 class AllAnnouncementListAPIView(ListAPIView):
