faaec3b1317b08ddeec926bfc05300f3907d9796
robsta7 <meremeev2001@gmail.com>
2021-01-02 13:02:47 +0300

    Update views

--- announcements/views.py
+++ announcements/views.py
@@ -1,12 +1,6 @@
-from drf_yasg.openapi import IN_PATH
-from drf_yasg.openapi import IN_QUERY
-from drf_yasg.openapi import Parameter
-from drf_yasg.openapi import TYPE_INTEGER
-from drf_yasg.utils import swagger_auto_schema
 from rest_framework.generics import CreateAPIView
 from rest_framework.generics import ListAPIView
 from rest_framework.generics import RetrieveDestroyAPIView
-from rest_framework.permissions import AllowAny
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Announcement
@@ -19,108 +13,26 @@ from .serializers import MapInfoSerializer
 class AllAnnouncementsListAPIView(ListAPIView):
     """Все объявления."""
 
-    permission_classes = (AllowAny, )
     serializer_class = AnnouncementSerializer
     queryset = Announcement.objects.all()
-    url_kwargs = ("page", )
-
-    @swagger_auto_schema(
-        operation_summary="Список всех объявлений.",
-        operation_description="Возвращает список всех объявлений.",
-        manual_parameters=[
-            Parameter(
-                url_kwargs[0],
-                IN_QUERY,
-                type=TYPE_INTEGER,
-                description="Номер страницы в наборе результатов с пагинацией.",
-            ),
-        ],
-        responses={
-            "200": AnnouncementSerializer,
-            "404": "Неправильная страница."
-        },
-    )
-    def get(self, request, *args, **kwargs):
-        return super(AllAnnouncementsListAPIView,
-                     self).get(request, *args, **kwargs)
 
 
 class UserAnnouncementsListAPIView(ListAPIView):
     """Объявления пользователя."""
 
-    permission_classes = (AllowAny, )
     serializer_class = AnnouncementSerializer
-    url_kwargs = ("user_id", "page")
-
-    @swagger_auto_schema(
-        operation_summary="Список объявлений пользователя.",
-        operation_description="""
-        Возвращает список объявлений пользователя по указанному id пользователя.
-        Сюда входят только те объявления, которые были созданы указанным пользователем.
-        """,
-        manual_parameters=[
-            Parameter(
-                url_kwargs[0],
-                IN_PATH,
-                type=TYPE_INTEGER,
-                description="Уникальный идентификатор пользователя.",
-            ),
-            Parameter(
-                url_kwargs[1],
-                IN_QUERY,
-                type=TYPE_INTEGER,
-                description="Номер страницы в наборе результатов с пагинацией.",
-            ),
-        ],
-        responses={
-            "200": AnnouncementSerializer,
-            "404": "Неправильная страница."
-        },
-    )
-    def get(self, request, *args, **kwargs):
-        return super(UserAnnouncementsListAPIView,
-                     self).get(request, *args, **kwargs)
+    lookup_url_kwarg = "user_id"
 
     def get_queryset(self):
         return Announcement.objects.filter(
-            user_id=self.kwargs.get(self.url_kwargs[0]))
+            user_id=self.kwargs.get(self.lookup_url_kwarg))
 
 
 class FeedAnnouncementsListAPIView(ListAPIView):
-    """Лента объявлений."""
+    """Лента объявлений для заданного пользователя."""
 
-    permission_classes = (AllowAny, )
     serializer_class = AnnouncementSerializer
-    url_kwargs = ("user_id", "page")
-
-    @swagger_auto_schema(
-        operation_summary="Лента объявлений.",
-        operation_description="""
-        Возвращает ленту объявлений для пользователя с указанным id.
-        Сюда не входят объявления, созданные указанным пользователем.
-        """,
-        manual_parameters=[
-            Parameter(
-                url_kwargs[0],
-                IN_PATH,
-                type=TYPE_INTEGER,
-                description="Уникальный идентификатор пользователя.",
-            ),
-            Parameter(
-                url_kwargs[1],
-                IN_QUERY,
-                type=TYPE_INTEGER,
-                description="Номер страницы в наборе результатов с пагинацией.",
-            ),
-        ],
-        responses={
-            "200": AnnouncementSerializer,
-            "404": "Неправильная страница."
-        },
-    )
-    def get(self, request, *args, **kwargs):
-        return super(FeedAnnouncementsListAPIView,
-                     self).get(request, *args, **kwargs)
+    lookup_url_kwarg = "user_id"
 
     def get_queryset(self):
         return Announcement.objects.exclude(
@@ -134,19 +46,6 @@ class AnnouncementCreateAPIView(CreateAPIView):
     serializer_class = AnnouncementCreateSerializer
     queryset = Announcement.objects.all()
 
-    @swagger_auto_schema(
-        operation_summary="Создание объявления.",
-        operation_description="Создаёт новое объявление.",
-        responses={
-            "201": AnnouncementSerializer,
-            "400": "Не заполнено обязательное поле.",
-            "403": "Учетные данные не были предоставлены.",
-        },
-    )
-    def post(self, request, *args, **kwargs):
-        return super(AnnouncementCreateAPIView,
-                     self).post(request, *args, **kwargs)
-
     def perform_create(self, serializer):
         return serializer.save(user=self.request.user)
 
@@ -157,53 +56,6 @@ class AnnouncementRetrieveDestroyAPIView(RetrieveDestroyAPIView):
     serializer_class = AnnouncementSerializer
     permission_classes = (IsAnnouncementAuthorOrReadOnly, )
     queryset = Announcement.objects.all()
-    url_kwargs = ("id", )
-
-    @swagger_auto_schema(
-        operation_summary="Получение объявления.",
-        operation_description=
-        "Возвращает объявление с указанным идентификатором.",
-        manual_parameters=[
-            Parameter(
-                url_kwargs[0],
-                IN_PATH,
-                type=TYPE_INTEGER,
-                description="Уникальный идентификатор объявления.",
-            )
-        ],
-        responses={
-            "200": AnnouncementSerializer,
-            "404": "Объявление не найдено."
-        },
-    )
-    def get(self, request, *args, **kwargs):
-        return super(AnnouncementRetrieveDestroyAPIView,
-                     self).get(request, *args, **kwargs)
-
-    @swagger_auto_schema(
-        operation_summary="Удаление объявления.",
-        operation_description="Удаляет объявление с указанным идентификатором.",
-        manual_parameters=[
-            Parameter(
-                "id",
-                IN_PATH,
-                type=TYPE_INTEGER,
-                description="Уникальный идентификатор объявления.",
-            )
-        ],
-        responses={
-            "204": "Объявление успешно удалено.",
-            "403": """
-                Учетные данные не были предоставлены.
-
-                У вас недостаточно прав для выполнения данного действия.
-                """,
-            "404": "Объявление не найдено.",
-        },
-    )
-    def delete(self, request, *args, **kwargs):
-        return super(AnnouncementRetrieveDestroyAPIView,
-                     self).delete(request, *args, **kwargs)
 
 
 class AllMapInfoListAPIView(ListAPIView):
@@ -212,18 +64,9 @@ class AllMapInfoListAPIView(ListAPIView):
     Это нужно для маркеров на карте объявлений.
     """
 
-    permission_classes = (AllowAny, )
     serializer_class = MapInfoSerializer
     queryset = Announcement.objects.all()
     pagination_class = None
-
-    @swagger_auto_schema(
-        operation_summary='Список всех объектов вида "id, широта, долгота".',
-        operation_description=
-        'Возвращает список всех объектов вида "id, широта, долгота".',
-    )
-    def get(self, request, *args, **kwargs):
-        return super(AllMapInfoListAPIView, self).get(request, *args, **kwargs)
 
 
 class FeedMapInfoListAPIView(ListAPIView):
@@ -232,29 +75,10 @@ class FeedMapInfoListAPIView(ListAPIView):
     Это нужно для маркеров на карте объявлений.
     """
 
-    permission_classes = (AllowAny, )
     serializer_class = MapInfoSerializer
+    lookup_url_kwarg = "user_id"
     pagination_class = None
-    url_kwargs = ("user_id", )
-
-    @swagger_auto_schema(
-        operation_summary=
-        'Список всех объектов вида "id, широта, долгота" для заданного пользователя.',
-        operation_description=
-        'Возвращает список всех объектов вида "id, широта, долгота" кроме созданных самим пользователем.',
-        manual_parameters=[
-            Parameter(
-                url_kwargs[0],
-                IN_PATH,
-                type=TYPE_INTEGER,
-                description="Уникальный идентификатор пользователя.",
-            ),
-        ],
-    )
-    def get(self, request, *args, **kwargs):
-        return super(FeedMapInfoListAPIView, self).get(request, *args,
-                                                       **kwargs)
 
     def get_queryset(self):
         return Announcement.objects.exclude(
-            user_id=self.kwargs.get(self.url_kwargs[0]))
+            user_id=self.kwargs.get(self.lookup_url_kwarg))
--- users/views.py
+++ users/views.py
@@ -1,40 +1,19 @@
-from drf_yasg.utils import swagger_auto_schema
 from rest_framework.generics import GenericAPIView
-from rest_framework.permissions import AllowAny
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 from rest_framework.status import HTTP_201_CREATED
 from rest_framework.status import HTTP_204_NO_CONTENT
-from rest_framework_simplejwt.views import TokenRefreshView
 
-from .serializers import SignInSerializer
-from .serializers import SignOutSerializer
-from .serializers import SignUpSerializer
+from users.serializers import SignInSerializer
+from users.serializers import SignOutSerializer
+from users.serializers import SignUpSerializer
 
 
 class SignUpAPIView(GenericAPIView):
     """Регистрация нового пользователя."""
 
     serializer_class = SignUpSerializer
-    permission_classes = (AllowAny, )
 
-    @swagger_auto_schema(
-        operation_summary="Регистрирация пользователя.",
-        operation_description="Регистрирует нового пользователя.",
-        responses={
-            "201":
-            SignUpSerializer,
-            "400":
-            """
-                Пользователь с таким именем или адресом электронной почты уже существует.
-
-                Неверный формат имени и/или адреса электронной почты.
-
-                Слишком короткий пароль, имя пользователя или адрес электронной почты.
-                """,
-        },
-    )
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -46,16 +25,7 @@ class SignInAPIView(GenericAPIView):
     """Вход в профиль."""
 
     serializer_class = SignInSerializer
-    permission_classes = (AllowAny, )
 
-    @swagger_auto_schema(
-        operation_summary="Авторизация пользователя.",
-        operation_description="Производит авторизацию пользователя.",
-        responses={
-            "200": SignInSerializer,
-            "403": "Неверный адрес электронной почты или пароль.",
-        },
-    )
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -66,35 +36,9 @@ class SignOutAPIView(GenericAPIView):
     """Выход из профиля."""
 
     serializer_class = SignOutSerializer
-    permission_classes = (IsAuthenticated, )
 
-    @swagger_auto_schema(
-        operation_summary="Выход из профиля пользователя.",
-        operation_description="Производит выход из профиля.",
-        responses={
-            "204": "Это успех, refresh токен сброшен.",
-            "400": "Недопустимый токен.",
-            "403": "Учетные данные не были предоставлены.",
-        },
-    )
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(status=HTTP_204_NO_CONTENT)
-
-
-class TokenRefreshAPIView(TokenRefreshView):
-    """Обновление access токена по refresh токену."""
-
-    @swagger_auto_schema(
-        operation_summary="Обновление токена доступа.",
-        operation_description=
-        "Обновляет access токен по заданному refresh токену.",
-        responses={
-            "200": "Это успех.",
-            "401": "Токен недействителен или просрочен."
-        },
-    )
-    def post(self, request, *args, **kwargs):
-        return super(TokenRefreshAPIView, self).post(request, *args, **kwargs)
