b7357dd736d57e83f82dbb7ff313366411727f4b
robsta7 <meremeev2001@gmail.com>
2021-01-02 13:03:06 +0300

    Update urls

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from .views import SignInAPIView
 from .views import SignOutAPIView
 from .views import SignUpAPIView
-from .views import TokenRefreshAPIView
 
 urlpatterns = [
     path("signup/", SignUpAPIView.as_view(), name="sign-up"),
     path("signin/", SignInAPIView.as_view(), name="sign-in"),
     path("signout/", SignOutAPIView.as_view(), name="sign-out"),
-    path("token/refresh/", TokenRefreshAPIView.as_view(),
+    path("token/refresh/", TokenRefreshView.as_view(),
          name="refresh-token"),
 ]
