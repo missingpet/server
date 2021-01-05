3f4102b9bc7f8a3dad71def41116d7b1447c0600
robsta7 <meremeev2001@gmail.com>
2021-01-05 03:27:35 +0300

    Safe method check

--- announcements/permissions.py
+++ announcements/permissions.py
@@ -1,8 +1,9 @@
 from rest_framework.permissions import BasePermission
+from rest_framework.permissions import SAFE_METHODS
 
 
 class IsAnnouncementAuthorOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method == "GET":
+        if request.method in SAFE_METHODS:
             return True
         return obj.user == request.user
