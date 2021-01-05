44ea08ed3cf8b1c30393d99e0f6610acba74e19d
robsta7 <60821327+robsta7@users.noreply.github.com>
2021-01-05 06:33:44 +0300

    Reorder imports

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from rest_framework.generics import GenericAPIView, CreateAPIView
+from rest_framework.generics import GenericAPIView
+from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 from rest_framework.status import HTTP_204_NO_CONTENT
