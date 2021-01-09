37395c91d4b7adeb8578508eb971f0292dfbcf62
Restyled.io <commits@restyled.io>
2021-01-09 11:51:00 +0000

    Restyled by reorder-python-imports

--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
-from rest_framework.generics import CreateAPIView, GenericAPIView
+from rest_framework.generics import CreateAPIView
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_204_NO_CONTENT
+from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_204_NO_CONTENT
 
-from .serializers import SignInSerializer, SignOutSerializer, SignUpSerializer
+from .serializers import SignInSerializer
+from .serializers import SignOutSerializer
+from .serializers import SignUpSerializer
 
 
 class SignUpAPIView(CreateAPIView):
