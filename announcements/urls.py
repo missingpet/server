e2edbc4f707c68135018b5e1c5f3b972ece09b6c
Restyled.io <commits@restyled.io>
2021-01-09 11:50:53 +0000

    Restyled by black

--- announcements/urls.py
+++ announcements/urls.py
@@ -14,9 +14,7 @@ user_urls = [
         UserAnnouncementsListAPIView.as_view(),
         name="user-announcements",
     ),
-    path("<int:user_id>/feed/",
-         FeedForUserListAPIView.as_view(),
-         name="feed-for-user"),
+    path("<int:user_id>/feed/", FeedForUserListAPIView.as_view(), name="feed-for-user"),
     path(
         "<int:user_id>/announcements_map/",
         AnnouncementsMapForUserListAPIView.as_view(),
@@ -30,9 +28,7 @@ announcement_urls = [
         AnnouncementRetrieveDestroyAPIView.as_view(),
         name="retrieve-destroy-announcement",
     ),
-    path("",
-         AnnouncementListCreateAPIView.as_view(),
-         name="list-create-announcement"),
+    path("", AnnouncementListCreateAPIView.as_view(), name="list-create-announcement"),
 ]
 
 announcements_map_urls = [
