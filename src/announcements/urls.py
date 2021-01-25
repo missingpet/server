7d72b282ca441e37bde74722b7e0db0696fa1ba0
robsta7 <meremeev2001@gmail.com>
2021-01-26 00:03:13 +0300

    Update urls

--- src/announcements/urls.py
+++ src/announcements/urls.py
@@ -1,46 +1,43 @@
 from django.urls import include
 from django.urls import path
 
-from .views import AnnouncementListCreateAPIView
-from .views import AnnouncementRetrieveDestroyAPIView
-from .views import AnnouncementsMapForUserListAPIView
-from .views import AnnouncementsMapListAPIView
-from .views import FeedForUserListAPIView
-from .views import UserAnnouncementsListAPIView
+from .views import AnnouncementViewSet, \
+    AnnouncementsMapForUserListAPIView, AnnouncementsMapListAPIView, \
+    FeedForUserListAPIView, UserAnnouncementsListAPIView
 
 user_urls = [
     path(
-        "<int:user_id>/announcements/",
+        '<int:user_id>/announcements/',
         UserAnnouncementsListAPIView.as_view(),
-        name="user-announcements",
+        name='user-announcements'
     ),
-    path("<int:user_id>/feed/",
+    path('<int:user_id>/feed/',
          FeedForUserListAPIView.as_view(),
-         name="feed-for-user"),
+         name='feed-for-user'),
     path(
-        "<int:user_id>/announcements_map/",
+        '<int:user_id>/announcements_map/',
         AnnouncementsMapForUserListAPIView.as_view(),
-        name="announcements-map-for-user",
-    ),
+        name='announcements-map-for-user'),
 ]
 
 announcement_urls = [
     path(
-        "<int:pk>/",
-        AnnouncementRetrieveDestroyAPIView.as_view(),
-        name="retrieve-destroy-announcement",
+        '<int:pk>/',
+        AnnouncementViewSet.as_view({'delete': 'destroy', 'get': 'retrieve'}),
+        name='retrieve-destroy-announcement'
     ),
-    path("",
-         AnnouncementListCreateAPIView.as_view(),
-         name="list-create-announcement"),
+
+    path('',
+         AnnouncementViewSet.as_view({'post': 'create', 'get': 'list'}),
+         name='create-announcement'),
 ]
 
 announcements_map_urls = [
-    path("", AnnouncementsMapListAPIView.as_view(), name="announcements-map"),
+    path('', AnnouncementsMapListAPIView.as_view(), name='announcements-map'),
 ]
 
 urlpatterns = [
-    path("user/", include(user_urls)),
-    path("announcement/", include(announcement_urls)),
-    path("announcements_map/", include(announcements_map_urls)),
+    path('user/', include(user_urls)),
+    path('announcement/', include(announcement_urls)),
+    path('announcements_map/', include(announcements_map_urls)),
 ]
