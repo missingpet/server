915535ab189456b96351a849b952dff02f13b9f9
robsta7 <meremeev2001@gmail.com>
2021-01-26 00:00:04 +0300

    Update models

--- src/announcements/models.py
+++ src/announcements/models.py
@@ -1,12 +1,6 @@
-from config.settings import ANNOUNCEMENTS_PHOTO
-from django.db.models import CASCADE
-from django.db.models import CharField
-from django.db.models import DateTimeField
-from django.db.models import FloatField
-from django.db.models import ForeignKey
-from django.db.models import ImageField
-from django.db.models import IntegerField
-from django.db.models import Model
+from django.conf import settings
+from django.db.models import CASCADE, CharField, DateTimeField, FloatField, \
+    ForeignKey, ImageField, IntegerField, Model
 from django.utils.translation import gettext_lazy as _
 from users.models import User
 
@@ -21,13 +15,12 @@ class Announcement(Model):
     DOGS = 1
     CATS = 2
     OTHERS = 3
-    ANIMAL_TYPES = ((DOGS, _("Собаки")), (CATS, _("Кошки")), (OTHERS,
-                                                              _("Иные")))
+    ANIMAL_TYPES = ((DOGS, _("Собаки")), (CATS, _("Кошки")), (OTHERS, _("Иные")))
 
     user = ForeignKey(User, on_delete=CASCADE, verbose_name=_("Пользователь"))
     description = CharField(_("Описание"), max_length=5000)
-    photo = ImageField(_("Фотография животного"),
-                       upload_to=ANNOUNCEMENTS_PHOTO)
+    photo = ImageField(_('Фотография животного'),
+                       upload_to=settings.ANNOUNCEMENTS_PHOTO)
     announcement_type = IntegerField(_("Тип объявления"),
                                      choices=ANNOUNCEMENT_TYPES)
     animal_type = IntegerField(_("Тип животного"), choices=ANIMAL_TYPES)
@@ -42,19 +35,6 @@ class Announcement(Model):
         ordering = ("-created_at", )
         verbose_name = _("Объявление")
         verbose_name_plural = _("Объявления")
-
-    def save(self, *args, **kwargs):
-        # При обновлении фотографии старую фотографию удаляем.
-        this_record = Announcement.objects.filter(id=self.id).first()
-        if this_record:
-            if this_record.photo != self.photo:
-                this_record.photo.delete(save=False)
-        super(Announcement, self).save(*args, **kwargs)
-
-    def delete(self, *args, **kwargs):
-        # Удаляем фотографию вместе с объявлением.
-        self.photo.delete(save=False)
-        super(Announcement, self).delete(*args, **kwargs)
 
     def __str__(self):
         return str(self.user)
