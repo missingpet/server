a1dd4c6ec6df6f05bebdda393fa53db502694c92
robsta7 <meremeev2001@gmail.com>
2021-01-26 00:06:30 +0300

    Update config

--- src/config/settings/production.py
+++ src/config/settings/production.py
@@ -6,7 +6,7 @@ ALLOWED_HOSTS = environ['ALLOWED_HOSTS'].split()
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": environ["DB_NAME"],
         "USER": environ["DB_USER"],
         "PASSWORD": environ["DB_PASSWORD"],
