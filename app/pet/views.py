a357d5271624097395a2c1906435eb5def908acb
Restyled.io <commits@restyled.io>
2021-02-06 16:23:01 +0000

    Restyled by reorder-python-imports

--- app/pet/views.py
+++ app/pet/views.py
@@ -1,8 +1,10 @@
-from rest_framework import generics, viewsets
+from rest_framework import generics
+from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from . import models, serializers
+from . import models
+from . import serializers
 from .pagination_service import AnnouncementPagination
 from .permissions import AnnouncementPermission
 
