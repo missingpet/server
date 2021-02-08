f3086492101161ede97d90da6270b485cbab0b9a
robsta7 <meremeev2001@gmail.com>
2021-02-08 21:54:39 +0300

    Update forms

--- app/pet/forms.py
+++ app/pet/forms.py
@@ -1,40 +1,15 @@
-from django import forms
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
-from . import models
+from .models import User
 
 
-class UserAdminForm(forms.ModelForm):
-    """Form which uses to provide more detailed\
-    information for \"User\" model in admin site."""
+class UserCreationCustomForm(UserCreationForm):
     class Meta:
-        model = models.User
-        fields = "__all__"
-        help_texts = {
-            "is_active": "Вместо удаления аккауна отключите этот пункт.",
-            "is_staff":
-            "Может ли пользователь заходить в административную панель.",
-            "is_superuser":
-            "Имеет ли пользователь все права без их явного назначения.",
-            "nickname": "Никнеймы не являются уникальными.",
-            "email":
-            "Адрес электронной почты является уникальным для каждого пользователя.",
-            "password": "Пароль хранится в виде хеша.",
-        }
+        model = User
+        fields = ('email', 'nickname')
 
 
-class AnnouncementAdminForm(forms.ModelForm):
-    """Form which uses to provide more detailed\
-    information for \"Announcement\" model in admin site."""
+class UserChangeCustomForm(UserChangeForm):
     class Meta:
-        model = models.Announcement
-        fields = "__all__"
-        help_texts = {
-            "user":
-            "Автор объявления.",
-            "description":
-            "Подробное описание объявления.",
-            "contact_phone_number":
-            "Номер телефона, по которому можно связаться с автором объявления.",
-            "address":
-            "Это адрес, по которому животное было найдено или потеряно.",
-        }
+        model = User
+        fields = ('email', 'nickname')
