9a592b4f6ffe4b3850665288f7a04e9c790355f5
Restyled.io <commits@restyled.io>
2021-03-08 15:50:47 +0000

    Restyled by isort

--- app/pet/views.py
+++ app/pet/views.py
@@ -1,10 +1,8 @@
-from rest_framework import generics
-from rest_framework import viewsets
+from rest_framework import generics, viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainSlidingView
 
-from . import models
-from . import serializers
+from . import models, serializers
 from .pagination import AnnouncementPagination
 from .permissions import AnnouncementPermission
 
