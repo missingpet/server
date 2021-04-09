59d041d6e5f0ef3987913c1ab87edd81ef5c53cc
robsta7 <meremeev2001@gmail.com>
2021-04-09 21:10:27 +0300

    refactor: change naming

--- app/pet/exceptions.py
+++ app/pet/exceptions.py
@@ -1,19 +1,21 @@
+from functools import wraps
 from smtplib import SMTPException
 
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
 
 
-def catch_email_message_exception_for_views(func):
+def catch_smtp_exception_for_view(func):
     """
-    Декоратор для отлавливания ошибок при обращения к почтовому сервису.
+    Декоратор для отлавливания ошибок при обращению к smtp сервису.
     Оборачивает исключение в 400 ответ во view.
     """
+    @wraps(func)
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
         except SMTPException as ex:
-            message = {"email_send_error": str(ex)}
+            message = {'smtp_error': str(ex)}
             return Response(status=HTTP_400_BAD_REQUEST, data=message)
 
     return wrapper
