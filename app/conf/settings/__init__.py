e70d946e2cbcee1d794b3264d10fa26c7c3b6d9d
robsta7 <meremeev2001@gmail.com>
2021-04-11 17:40:38 +0300

    config: set default django environment

--- app/conf/settings/__init__.py
+++ app/conf/settings/__init__.py
@@ -4,14 +4,14 @@ from django.core.exceptions import ImproperlyConfigured
 ENV_PRODUCTION = "production"
 ENV_DEVELOPMENT = "development"
 
-APPLICATION_ENVIRONMENT = decouple.config("DJANGO_ENV")
+DJANGO_ENV = decouple.config("DJANGO_ENV", default="development")
 
 ENVIRONMENTS = (ENV_PRODUCTION, ENV_DEVELOPMENT)
 
-if APPLICATION_ENVIRONMENT not in ENVIRONMENTS:
+if DJANGO_ENV not in ENVIRONMENTS:
     raise ImproperlyConfigured("Invalid DJANGO_ENV setting")
 
-if APPLICATION_ENVIRONMENT == ENV_DEVELOPMENT:
+if DJANGO_ENV == ENV_DEVELOPMENT:
     from .development import *  # NOQA
-elif APPLICATION_ENVIRONMENT == ENV_PRODUCTION:
+elif DJANGO_ENV == ENV_PRODUCTION:
     from .production import *  # NOQA
