79eb3d95d4c12f3cea8135bce17917aed23846c8
Kir_02 <koksharov-kir@yandex.ru>
2021-04-12 12:36:02 +0300

    add data factories

--- app/pet/tests/data_factories.py
+++ app/pet/tests/data_factories.py
@@ -7,3 +7,18 @@ from .. import models
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.User
+
+
+class PasswordResetConfirmationCodeFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.PasswordResetConfirmationCode
+
+    user = factory.SubFactory(UserFactory)
+
+
+class AnnouncementFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Announcement
+
+    user = factory.SubFactory(UserFactory)
+    photo = factory.django.ImageField()
