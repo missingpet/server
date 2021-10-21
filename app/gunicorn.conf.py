2f20a17c99de286ab48d066a7d6478baaa53aba4
Mikhail Eremeev <meremeev@sfedu.ru>
2021-10-21 20:27:29 +0300

    refactor: gunicorn_config.py -> gunicorn.conf.py

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -4,8 +4,8 @@ Gunicorn configuration module.
 from multiprocessing import cpu_count
 
 workers = 2 * cpu_count() + 1
-bind = "0.0.0.0:8000"
+bind = '0.0.0.0:8000'
 limit_request_fields = 32768
 limit_request_field_size = 0
-forwarded_allow_ips = "*"
-raw_env = "DJANGO_SETTINGS_MODULE=conf.settings"
+forwarded_allow_ips = '*'
+raw_env = 'DJANGO_SETTINGS_MODULE=conf.settings'
