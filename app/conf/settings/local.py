537403ccd4357688bcd28e0d3cf6bb7594741009
Mikhail Eremeev <meremeev@sfedu.ru>
2021-10-27 19:57:32 +0300

    fix: set correct db name

--- app/conf/settings/local.py
+++ app/conf/settings/local.py
@@ -12,7 +12,7 @@ ALLOWED_HOSTS = decouple.config(
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': decouple.config('POSTGRES_NAME'),
+        'NAME': decouple.config('POSTGRES_DB'),
         'USER': decouple.config('POSTGRES_USER'),
         'PASSWORD': decouple.config('POSTGRES_PASSWORD'),
         'HOST': decouple.config('POSTGRES_HOST'),
